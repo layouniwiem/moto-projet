7faac5527ebb77bf9c7f81807c236554d9ec8d51
layouniwiem <layouniwiem1993@gmail.com>
2025-02-20 11:06:30 +0100

    fix  issues

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
-# __init__.py
-from flask import Flask
+# __init__.pyfrom flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from .config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
 
 def create_app():
     app = Flask(__name__)
