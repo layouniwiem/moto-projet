badb9d14646434c41e6fa6ffe0b80060fc3ce9e5
layouniwiem <layouniwiem1993@gmail.com>
2025-06-13 17:22:52 +0200

    Mise à jour jenkins et add test

--- test/test_home.py
+++ test/test_home.py
@@ -1,8 +1,24 @@
-from app import create_app
+#from app import create_app
+
+#def test_homepage():
+#    app = create_app()
+ #   client = app.test_client()
+  #  response = client.get('/')
+   # assert response.status_code == 200
+    #assert b"Motos" in response.data  # Ajuste selon le contenu attendu
+from app import create_app, db
 
 def test_homepage():
-    app = create_app()
-    client = app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"Motos" in response.data  # Ajuste selon le contenu attendu
+    app = create_app({
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+        "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+    })
+
+    with app.test_client() as client:
+        with app.app_context():
+            db.create_all()
+
+        response = client.get('/')
+        assert response.status_code == 200
+        assert b"Motos" in response.data
