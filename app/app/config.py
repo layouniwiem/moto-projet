80e8cf8657653ec1638fd4d21db4ce9ed173fc92
layouniwiem <layouniwiem1993@gmail.com>
2025-06-15 20:46:31 +0200

    fix config.py

--- app/app/config.py
+++ app/app/config.py
@@ -7,5 +7,5 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-à-changer-en-prod'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+pymysql://user:password@localhost/moto_db'
+        'mysql+pymysql://moto_user:moto_password@localhost/moto_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
