003f7267fbd04d25637cd49a35c9261c946954e8
layouniwiem <layouniwiem1993@gmail.com>
2025-06-18 16:49:25 +0200

    fix add prometeus config

--- app/app/config.py
+++ app/app/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-à-changer-en-prod'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('mysql+pymysql://moto_user:moto_password@mariadb:3306/moto_db') or \
         'mysql+pymysql://moto_user:moto_password@mariadb:3306/moto_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
