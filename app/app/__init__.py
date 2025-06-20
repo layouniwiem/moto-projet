d9f54262b7910fd6e71238803c29c35057777707
layouniwiem <layouniwiem1993@gmail.com>
2025-06-21 00:28:37 +0200

    fix init

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -45,8 +45,8 @@ def create_app(test_config=None):
 
     #from monitoring import monitoring
     #monitoring.register_metrics(app)
-    from monitoring import setup_metrics
-    setup_metrics(app)
+   # from monitoring import setup_metrics
+    #setup_metrics(app)
     os.makedirs(os.path.join(app.static_folder, 'img'), exist_ok=True)
 
     return app
